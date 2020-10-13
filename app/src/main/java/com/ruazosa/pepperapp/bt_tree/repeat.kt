c44544cbca0529785f0d772fc1f320629762806b
Borna Zbodulja <borna.zbodulja@gmail.com>
2020-10-13 22:53:47 +0200

    updated saySeeYou and repeat functions

--- app/src/main/java/com/ruazosa/pepperapp/bt_tree/repeat.kt
+++ app/src/main/java/com/ruazosa/pepperapp/bt_tree/repeat.kt
@@ -1,11 +1,24 @@
 package com.ruazosa.pepperapp.bt_tree
 
+import android.util.Log
 import com.aldebaran.qi.sdk.QiContext
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 fun repeat(qiContext: QiContext){
     GlobalScope.launch {
-
+        Log.d("REPEAT_TAG", "repeat started")
+        while (true){
+            node3(qiContext)
+            if(Variables.nodes["Node3"] == true){
+                Variables.repeat = true
+                Log.d("REPEAT_TAG", "repeat true")
+            }else if(Variables.nodes["Node3"] == false){
+                if(Variables.executed && !Variables.invert){
+                    Variables.repeat = false
+                    Log.d("REPEAT_TAG", "repeat false")
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/ruazosa/pepperapp/bt_tree/seeYou.kt
+++ app/src/main/java/com/ruazosa/pepperapp/bt_tree/seeYou.kt
@@ -2,6 +2,13 @@ package com.ruazosa.pepperapp.bt_tree
 
 import android.util.Log
 import com.aldebaran.qi.sdk.QiContext
+import com.aldebaran.qi.sdk.`object`.conversation.BodyLanguageOption
+import com.aldebaran.qi.sdk.`object`.conversation.Phrase
+import com.aldebaran.qi.sdk.`object`.conversation.Say
+import com.aldebaran.qi.sdk.`object`.locale.Language
+import com.aldebaran.qi.sdk.`object`.locale.Locale
+import com.aldebaran.qi.sdk.`object`.locale.Region
+import com.aldebaran.qi.sdk.builder.SayBuilder
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
@@ -9,6 +16,17 @@ fun saySeeYou(qiContext: QiContext){
     GlobalScope.launch {
         Variables.saying += ("SeeYou" to false)
         Log.d("SEEYOU_TAG", "saySeeYou block started")
-        
+
+        val locale = Locale(Language.ENGLISH, Region.UNITED_STATES)
+
+        val say: Say = SayBuilder.with(qiContext)
+            .withPhrase(Phrase("Hi ${Variables.name}, it is nice to see you again at the Faculty."))
+            .withBodyLanguageOption(BodyLanguageOption.NEUTRAL)
+            .withLocale(locale)
+            .build()
+        say.run()
+
+        Variables.saying += ("SeeYou" to true)
+        Log.d("SEEYOU_TAG", "SeeYou block executed")
     }
 }
